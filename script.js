a3c015967ea1014b1f2bf4f5e07eac4b4fc839be
whoisthemetro <62461554+whoisthemetro@users.noreply.github.com>
2025-08-02 17:16:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const audio = document.getElementById("audio");
+const title = document.getElementById("song-title");
+const songs = [
+  { title: "Song 1", file: "assets/songs/song1.mp3" },
+  { title: "Song 2", file: "assets/songs/song2.mp3" },
+  { title: "Song 3", file: "assets/songs/song3.mp3" },
+];
+
+let index = 0;
+
+function loadSong(i) {
+  audio.src = songs[i].file;
+  title.textContent = songs[i].title;
+  audio.play();
+  pauseAllVideos();
+}
+
+function togglePlay() {
+  if (audio.paused) {
+    audio.play();
+    pauseAllVideos();
+  } else {
+    audio.pause();
+  }
+}
+
+function prevSong() {
+  index = (index - 1 + songs.length) % songs.length;
+  loadSong(index);
+}
+
+function nextSong() {
+  index = (index + 1) % songs.length;
+  loadSong(index);
+}
+
+function pauseAllVideos() {
+  document.querySelectorAll("video").forEach(v => v.pause());
+}
+
+document.querySelectorAll("video").forEach(video => {
+  video.addEventListener("play", () => {
+    pauseAllVideos();
+    video.play();
+    audio.pause();
+  });
+});
+
+function generateTracklist() {
+  const container = document.getElementById("tracklist");
+  songs.forEach((s, i) => {
+    const div = document.createElement("div");
+    div.innerText = s.title;
+    div.onclick = () => {
+      index = i;
+      loadSong(i);
+    };
+    container.appendChild(div);
+  });
+}
+
+generateTracklist();
+
+const titleEl = document.getElementById("siteTitle");
+titleEl.addEventListener("mouseover", () => {
+  const text = titleEl.innerText.split('');
+  titleEl.innerHTML = text.map(letter => {
+    const color = `hsl(${Math.random() * 360}, 100%, 70%)`;
+    return `<span style="color:${color}">${letter}</span>`;
+  }).join('');
+});
+titleEl.addEventListener("mouseout", () => {
+  titleEl.innerText = "whoistheMETRO?";
+});
